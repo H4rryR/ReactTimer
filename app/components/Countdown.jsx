98a319277e2fc385ca33f0a55b91e4d148377c75
Harry Radford <harry.radford@live.co.uk>
2017-01-29 14:32:48 +0000

    Add nav

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = (props) => {
+  return (
+    <div>
+      <h2>Countdown Component</h2>
+    </div>
+  );
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = (props) => {
+  return (
+    <div>
+      <h2>Timer Component</h2>
+    </div>
+  );
+}
+
+module.exports = Timer;
