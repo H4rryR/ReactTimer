7b821fa43324596d5b81cb4fc73b0435287b3ffe
Harry Radford <harry.radford@live.co.uk>
2017-01-31 22:40:19 +0000

    Style clock

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var ReactDom = require('react-dom');
+var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
@@ -9,6 +9,16 @@ var Clock = require('Clock');
 describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
+  });
+
+  describe('render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={65} />);
+      var $el = $(ReactDOM.findDOMNode(clock)); //Converts Clock component to HTML before passing it though jQuery
+      var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:05');
+    });
   });
 
   describe('formatSeconds', () => {
@@ -21,7 +31,7 @@ describe('Clock', () => {
       expect(actual).toBe(expected);
     });
 
-    it('should format seconds when mins or secs is < 10', () => {
+    it('should format seconds when mins or secs is < 10 (edge-case)', () => {
       var clock = TestUtils.renderIntoDocument(<Clock />);
       var seconds = 61;
       var expected = '01:01';
